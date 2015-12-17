245a6e0dd5e816be6a52768f93ef35e1972fae92
Sean Larkin <sean.larkin@mutualofomaha.com>
2015-12-17 10:58:32 -0600

    Initial Commit trying new webpack config

--- src/index.js
+++ src/index.js
@@ -1,6 +1,32 @@
 import mooInputDirective from './mooInput.js';
 
-var moduleName = 'mooAngular.input';
-var mod = angular.module(moduleName, []);
 
-mooInputDirective(mod);
+(function (root, factory) {
+    if (typeof module !== 'undefined' && module.exports) {
+        // CommonJS
+        if (typeof angular === 'undefined') {
+            var angular = require('angular');
+            factory(angular);
+            module.exports = 'mooAngularInput';
+        } else {
+            factory(angular);
+            module.exports = 'mooAngularInput';
+        }
+    } else if (typeof define === 'function' && define.amd) {
+        // AMD
+        define(['angular'], factory);
+    } else {
+        // Global Variables
+        factory(root.angular);
+    }
+}(this, function (angular) {
+  'use strict';
+  // create your angular module and do stuff
+  var moduleName = 'mooAngular.input';
+  var mod = angular.module(moduleName, []);
+
+  mooInputDirective(mod);
+
+  return moduleName; // the name of your module
+
+}));
